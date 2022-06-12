9878735bda8969979527e5ed102fac252fa219d0
Aishwarya Sharma <1aishwaryasharma@gmail.com>
2022-06-12 10:20:12 +0000

    feat: monkey patch to get a mocked function

--- src/practice/monkey-patching.js
+++ src/practice/monkey-patching.js
@@ -2,5 +2,13 @@ const assert = require('assert');
 const thumbWar = require('../thumb-war');
 const utils = require('../utils');
 
+// keep a reference of the original so that it can be reset later on for other tests
+const originalGetWinner = utils.getWinner;
+
+// monkey patching
+utils.getWinner = (player1, player2) => player1;
+
 const winner = thumbWar('Aishwarya', 'Yasharth');
 assert.strictEqual(winner, 'Aishwarya');
+
+utils.getWinner = originalGetWinner;
